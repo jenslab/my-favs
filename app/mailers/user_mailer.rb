41299d379d44517902f5ae208aed315e2c898b3d
Jennifer <palacios.jennifer@gmail.com>
2015-03-28 16:10:36 +0100

    deleted configuration from user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,4 @@
 class UserMailer < ApplicationMailer
-  default from: 'palacios.jennifer@gmail.com'
  
-  def welcome_email(user)
-    @user = user
-    @url  = 'https://my-favs.herokuapp.com/users/sign_in'
-    mail(to: @user.email, subject: 'Welcome to My Favs!')
-  end
 end
-end
+
